ac7c7d789d32a0d7eb1e3d7add9f76ca6b5a8718
System Administrator <root@shitagarwal-ltm.internal.salesforce.com>
2019-10-30 22:23:00 -0400

    react app with segment tracking code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class SignupButton extends Component {
+  trackEvent() {
+    window.analytics.track('button-click',{eventId: this.props.eventId, eventClass: this.props.eventClass});
+  } render() {
+    return (
+      <button onClick={this.trackEvent.bind(this)}>
+        {this.props.children};
+      </button>
+    );
+  }
+}
+
+
+function clickHandlerB() {
+  // debugger;
+  window.analytics.track("clicked-button-x-from-b");
+}
+
 function App() {
+  const x = 100;
+  const arr = ["George", "Achin", "Shitij"];
+
+  const clickHandler = () => {
+    // debugger;
+    window.analytics.track("clicked-button-x");
+  };
+
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,11 +45,19 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn shit: {x}
         </a>
+        {arr.map(name => <div key={`friend-${name}`}>{name}</div>)}
+        <hr />
+        <SignupButton eventId="holler-btn" eventClass="btn">Holler!</SignupButton>
+        <hr />
+        <button onClick={clickHandlerB}>
+          Click me.
+        </button> 
       </header>
     </div>
   );
 }
 
 export default App;
+
